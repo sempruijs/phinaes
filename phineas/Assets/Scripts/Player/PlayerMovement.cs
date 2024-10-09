4eb680673c400bb7951f9b5f2f703f7e9bac870e
RaynisonTD <145349773+RaynisonTD@users.noreply.github.com>
2024-10-09 20:02:42 +0200

    tile map wederom opniew gemaakt (#24)
    we hebben een simpele tilemap gemaakt omdat unity en git issues hebben veroorzaakt omringende textures

--- phineas/Assets/Scripts/Player/PlayerMovement.cs
+++ phineas/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,74 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    enum Direction
-    {
-        up,
-        down,
-        left,
-        right
-    }
-
     public float moveSpeed = 5f;  // Speed of movement
-    public float gridSize = 0.2f; // Size of the grid for smoother movement
-    private Vector3 targetPosition;
-    private bool isMoving = false;
-
-    private Direction currentDirection;
-
-    private void Start()
-    {
-        targetPosition = transform.position;
-    }
+    private Vector3 movement;
 
     void Update()
     {
-        if (!isMoving)
-        {
-            GetInput();
-        }
-
+        GetInput();
         Move();
     }
 
     void GetInput()
     {
+        movement = Vector3.zero; // Reset movement vector
+
+        // Check for vertical input (W/S)
         if (Input.GetKey(KeyCode.W)) // Move up
         {
-            currentDirection = Direction.up;
-            targetPosition = transform.position + Vector3.up * gridSize;
-            isMoving = true;
+            movement = Vector3.up;
         }
         else if (Input.GetKey(KeyCode.S)) // Move down
         {
-            currentDirection = Direction.down;
-            targetPosition = transform.position + Vector3.down * gridSize;
-            isMoving = true;
+            movement = Vector3.down;
         }
+
+        // Check for horizontal input (A/D)
         else if (Input.GetKey(KeyCode.A)) // Move left
         {
-            currentDirection = Direction.left;
-            targetPosition = transform.position + Vector3.left * gridSize;
-            isMoving = true;
+            movement = Vector3.left;
         }
         else if (Input.GetKey(KeyCode.D)) // Move right
         {
-            currentDirection = Direction.right;
-            targetPosition = transform.position + Vector3.right * gridSize;
-            isMoving = true;
+            movement = Vector3.right;
         }
     }
 
     void Move()
     {
-        if (isMoving)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, targetPosition) <= 0.01f)
-            {
-                transform.position = targetPosition;
-                isMoving = false;
-            }
-        }
+        transform.position += movement * moveSpeed * Time.deltaTime; // Apply movement based on input
     }
 }
